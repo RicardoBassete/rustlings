2a36e227392cfabc060371a69f875aab204602a9
Ricardo Bassete <ricardogbassete@gmail.com>
2024-10-15 10:47:22 -0300

    finish rustlings

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -41,7 +41,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut data = s.split(',');
+        let (Some(name), Some(age), None) = (data.next(), data.next(), data.next()) else {
+            return Err(ParsePersonError::BadLen);
+        };
+
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let age = age.parse().map_err(ParsePersonError::ParseInt)?;
+
+        Ok(Self {
+            name: name.into(),
+            age,
+        })
+    }
 }
 
 fn main() {
